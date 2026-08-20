f018be8e11e8d6c51e57f452b4bda4e5eddd13d0
Wasim <wasimaahmad413@gmail.com>
2026-08-20 13:18:31 +0530

    changes

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -20,11 +20,12 @@ const Signup = () => {
         });
         const result = await response.json()
         if(result.success){
-            console.log(result.data)
-            console.log(result.message)
+            // console.log(result.data)
+            // console.log(result.message)
             toast.success(result.message)
-            setUserInfo(...updatedUserInfo)
-            navigate('/login')
+            setUserInfo(updatedUserInfo)
+            setTimeout(()=>{navigate('/login')},1000)
+            // navigate('/login')
         }
         else{
             console.log(result.message)
